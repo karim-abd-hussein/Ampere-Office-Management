73eaee5fbfcc7f12b291dba989dcd9870e7fb734
karim <karimabdhussein@gmail.com>
2026-05-02 07:30:15 -0700

    fix admin route and db name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,7 +24,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('')
+            ->path('/admin')
             ->brandName('مكتب الأمبير')
             ->login()
             ->colors([
